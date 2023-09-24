a4f236a782d840f488c0ff7a7229279d97613b67
SOUMYAJIT GHOSH <soumyajitghosh@SOUMYAJITs-MacBook-Pro.local>
2023-09-24 22:22:16 +0530

    Card Contact View

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
+import {
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Text,
+  Image,
+  StyleSheet,
+} from "react-native";
+import { Card, Divider } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 import { FontAwesome } from "@expo/vector-icons";
 import colors from "../colors";
@@ -41,6 +49,8 @@ const Home = () => {
     }
   }, [currentuser]);
 
+  console.log("Contacts:", contacts);
+
   useEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
@@ -64,15 +74,79 @@ const Home = () => {
     });
   }, [navigation]);
 
+  const users = [
+    {
+      name: "brynn",
+      avatar: "https://uifaces.co/our-content/donated/1H_7AxP0.jpg",
+    },
+    {
+      name: "thot leader",
+      avatar:
+        "https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb",
+    },
+    {
+      name: "jsa",
+      avatar: "https://uifaces.co/our-content/donated/bUkmHPKs.jpg",
+    },
+    {
+      name: "talhaconcepts",
+      avatar: "https://randomuser.me/api/portraits/men/4.jpg",
+    },
+    {
+      name: "andy vitale",
+      avatar: "https://uifaces.co/our-content/donated/NY9hnAbp.jpg",
+    },
+    {
+      name: "katy friedson",
+      avatar:
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgxMTc1MTYzM15BMl5BanBnXkFtZTgwNzI5NjMwOTE@._V1_UY256_CR16,0,172,256_AL_.jpg",
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        // onPress={() => navigation.navigate("Chat")}
-        style={styles.chatButton}
-      >
-        <Entypo name="chat" size={24} color={colors.lightGray} />
-      </TouchableOpacity>
-    </View>
+    // <View style={styles.container}>
+    //   {/* <Card containerStyle={{padding:0}}>
+    //   {contacts.map((contact, idx)=>
+    //     <ListItem
+    //     key={idx}
+    //     roundAvatar
+    //     title={contact.username}
+    //     avatar={{uri: contact.avatarImage}}
+    //   />)}
+    //   </Card> */}
+
+    //   <TouchableOpacity
+    //     // onPress={() => navigation.navigate("Chat")}
+    //     style={styles.chatButton}
+    //   >
+    //     <Entypo name="chat" size={24} color={colors.lightGray} />
+    //   </TouchableOpacity>
+    // </View>
+    <>
+      <ScrollView>
+        <View style={styles.container}>
+          <Card>
+            {contacts.map((contact, i) => {
+              return (
+                <>
+                  <View key={i} style={styles.user}>
+                    <Image
+                      style={styles.image}
+                      resizeMode="cover"
+                      source={{
+                        uri: "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d",
+                      }}
+                    />
+                    <Text style={styles.name}>{contact.username}</Text>
+                  </View>
+                  <Divider style={{ backgroundColor: "blue" }} />
+                </>
+              );
+            })}
+          </Card>
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
@@ -81,9 +155,24 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
+    // justifyContent: "flex-end",
+    // alignItems: "flex-end",
+    paddingTop: 20,
     backgroundColor: "#fff",
+  },
+  user: {
+    flexDirection: "row",
+    paddingVertical: 6,
+    marginBottom: 16,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    marginRight: 25,
+  },
+  name: {
+    fontSize: 24,
+    marginTop: 5,
   },
   chatButton: {
     backgroundColor: colors.primary,
