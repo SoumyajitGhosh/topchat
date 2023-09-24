93dfb5c2f1dae296c9de063903ba07335bee1cc2
SOUMYAJIT GHOSH <soumyajitghosh@SOUMYAJITs-MacBook-Pro.local>
2023-09-24 21:10:07 +0530

    Login and Signup APIs integrated

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,69 +1,105 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { FontAwesome } from '@expo/vector-icons';
-import colors from '../colors';
-import { Entypo } from '@expo/vector-icons';
-const catImageUrl = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
+import { FontAwesome } from "@expo/vector-icons";
+import colors from "../colors";
+import { Entypo } from "@expo/vector-icons";
+import { allUsersRoute } from "../utils/APIRoutes";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const catImageUrl =
+  "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
 
 const Home = () => {
+  const [currentuser, setCurrentuser] = useState(null);
+  const [contacts, setContacts] = useState([]);
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  const getStorageData = async () => {
+    try {
+      const savedUser = await AsyncStorage.getItem("user");
+      const currentUser = JSON.parse(savedUser);
+      setCurrentuser(currentUser);
+    } catch (error) {
+      await AsyncStorage.clear();
+      navigation.navigate("Login");
+    }
+  };
 
-    useEffect(() => {
-        navigation.setOptions({
-            headerLeft: () => (
-                <FontAwesome name="search" size={24} color={colors.gray} style={{marginLeft: 15}}/>
-            ),
-            headerRight: () => (
-                <Image
-                    source={{ uri: catImageUrl }}
-                    style={{
-                        width: 40,
-                        height: 40,
-                        marginRight: 15,
-                    }}
-                />
-            ),
-        });
-    }, [navigation]);
+  const fetchAllUsers = async (id) => {
+    const { data } = await axios.get(`${allUsersRoute}/${id}`);
+    setContacts(data);
+  };
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Chat")}
-                style={styles.chatButton}
-            >
-                <Entypo name="chat" size={24} color={colors.lightGray} />
-            </TouchableOpacity>
-        </View>
-    );
-    };
+  useEffect(() => {
+    getStorageData();
+  }, []);
 
-    export default Home;
+  useEffect(() => {
+    if (currentuser) {
+      fetchAllUsers(currentuser._id);
+    }
+  }, [currentuser]);
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'flex-end',
-            alignItems: 'flex-end',
-            backgroundColor: "#fff",
-        },
-        chatButton: {
-            backgroundColor: colors.primary,
-            height: 50,
-            width: 50,
-            borderRadius: 25,
-            alignItems: 'center',
-            justifyContent: 'center',
-            shadowColor: colors.primary,
-            shadowOffset: {
-                width: 0,
-                height: 2,
-            },
-            shadowOpacity: .9,
-            shadowRadius: 8,
-            marginRight: 20,
-            marginBottom: 50,
-        }
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <FontAwesome
+          name="search"
+          size={24}
+          color={colors.gray}
+          style={{ marginLeft: 15 }}
+        />
+      ),
+      headerRight: () => (
+        <Image
+          source={{ uri: catImageUrl }}
+          style={{
+            width: 40,
+            height: 40,
+            marginRight: 15,
+          }}
+        />
+      ),
     });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        // onPress={() => navigation.navigate("Chat")}
+        style={styles.chatButton}
+      >
+        <Entypo name="chat" size={24} color={colors.lightGray} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    backgroundColor: "#fff",
+  },
+  chatButton: {
+    backgroundColor: colors.primary,
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: colors.primary,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.9,
+    shadowRadius: 8,
+    marginRight: 20,
+    marginBottom: 50,
+  },
+});
--- client/screens/Login.js
+++ client/screens/Login.js
@@ -1,57 +1,99 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput, Image, SafeAreaView, TouchableOpacity, StatusBar, Alert } from "react-native";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../config/firebase";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  StatusBar,
+  Alert,
+} from "react-native";
+// import { signInWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "../config/firebase";
+import axios from "axios";
+import { loginRoute } from "../utils/APIRoutes";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const backImage = require("../assets/backImage.png");
 
 export default function Login({ navigation }) {
-
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const onHandleLogin = () => {
-    if (email !== "" && password !== "") {
-      signInWithEmailAndPassword(auth, email, password)
-        .then(() => console.log("Login success"))
-        .catch((err) => Alert.alert("Login error", err.message));
+  const setStorageData = async (data) => {
+    try {
+      await AsyncStorage.setItem("user", JSON.stringify(data));
+      navigation.navigate("Home");
+    } catch {
+      await AsyncStorage.clear();
     }
   };
-  
+
+  const onHandleLogin = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.post(loginRoute, { username, password });
+    if (data.status === false) {
+      console.log("Encountered error");
+    } else {
+      setStorageData(data.user);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image source={backImage} style={styles.backImage} />
       <View style={styles.whiteSheet} />
       <SafeAreaView style={styles.form}>
         <Text style={styles.title}>Log In</Text>
-         <TextInput
-        style={styles.input}
-        placeholder="Enter email"
-        autoCapitalize="none"
-        keyboardType="email-address"
-        textContentType="emailAddress"
-        autoFocus={true}
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Enter password"
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry={true}
-        textContentType="password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <TouchableOpacity style={styles.button} onPress={onHandleLogin}>
-        <Text style={{fontWeight: 'bold', color: '#fff', fontSize: 18}}> Log In</Text>
-      </TouchableOpacity>
-      <View style={{marginTop: 20, flexDirection: 'row', alignItems: 'center', alignSelf: 'center'}}>
-        <Text style={{color: 'gray', fontWeight: '600', fontSize: 14}}>Don't have an account? </Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-          <Text style={{color: '#f57c00', fontWeight: '600', fontSize: 14}}> Sign Up</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter username"
+          autoCapitalize="none"
+          // keyboardType="email-address"
+          textContentType="username"
+          autoFocus={true}
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={(e) => onHandleLogin(e)}
+        >
+          <Text style={{ fontWeight: "bold", color: "#fff", fontSize: 18 }}>
+            {" "}
+            Log In
+          </Text>
         </TouchableOpacity>
-      </View>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: "row",
+            alignItems: "center",
+            alignSelf: "center",
+          }}
+        >
+          <Text style={{ color: "gray", fontWeight: "600", fontSize: 14 }}>
+            Don't have an account?{" "}
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+            <Text style={{ color: "#f57c00", fontWeight: "600", fontSize: 14 }}>
+              {" "}
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
       <StatusBar barStyle="light-content" />
     </View>
@@ -64,7 +106,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 36,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: "orange",
     alignSelf: "center",
     paddingBottom: 24,
@@ -82,27 +124,27 @@ const styles = StyleSheet.create({
     height: 340,
     position: "absolute",
     top: 0,
-    resizeMode: 'cover',
+    resizeMode: "cover",
   },
   whiteSheet: {
-    width: '100%',
-    height: '75%',
+    width: "100%",
+    height: "75%",
     position: "absolute",
     bottom: 0,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderTopLeftRadius: 60,
   },
   form: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginHorizontal: 30,
   },
   button: {
-    backgroundColor: '#f57c00',
+    backgroundColor: "#f57c00",
     height: 58,
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 40,
   },
 });
